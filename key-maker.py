73e3eea555247cbaef61041615678b231b05f6d4
Sasoun Torossian <sasountorossian@gmail.com>
2020-11-24 17:46:45 +0000

    Added try/except block to check if key exists first.

--- key-maker.py
+++ key-maker.py
@@ -1,6 +1,8 @@
 import os, sys, ctypes
 import winreg as reg
 
+exists = False
+
 # Get path of current working directory and python.exe
 cwd = os.getcwd()
 python_exe = sys.executable
@@ -8,18 +10,23 @@ python_exe = sys.executable
 # Set the path of the context menu (right-click menu)
 key_path = r'Directory\\Background\\shell\\Movie Selector\\' # Change 'Organiser' to the name of your project
 
-# Checks if key make is run in admin mode. If not, re-run key-maker.py as admin.
-if ctypes.windll.shell32.IsUserAnAdmin():
-    # Create outer key
-    key = reg.CreateKey(reg.HKEY_CLASSES_ROOT, key_path)
-    reg.SetValue(key, '', reg.REG_SZ, "Movie Selector")  # Creates Movie Selector key
-    reg.SetValueEx(key, 'Icon', 0,  reg.REG_SZ, f'{cwd}\\film.ico')  # Creates icon key in Movie Selector key
+# Checks to see if key exists. If not, then proceed to create one.
+try: 
+    key = reg.OpenKey(reg.HKEY_CLASSES_ROOT, key_path)
+except: 
+    # Checks if key make is run in admin mode. If not, re-run key-maker.py as admin.
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        # Create outer key
+        key = reg.CreateKey(reg.HKEY_CLASSES_ROOT, key_path)
+        reg.SetValue(key, '', reg.REG_SZ, "Movie Selector")  # Creates Movie Selector key
+        reg.SetValueEx(key, 'Icon', 0,  reg.REG_SZ, f'{cwd}\\film.ico')  # Creates icon key in Movie Selector key
 
-    # create inner key
-    key2 = reg.CreateKey(key, r"command")
-    reg.SetValue(key2, '', reg.REG_SZ, python_exe + f' "{cwd}\\movie-selector.py"') # Creates command key and assigns it the python.exe and script file paths.
-else:
-    ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, 'key-maker.py', None, 1)
+        # create inner key
+        key2 = reg.CreateKey(key, r"command")
+        reg.SetValue(key2, '', reg.REG_SZ, python_exe + f' "{cwd}\\movie-selector.py"') # Creates command key and assigns it the python.exe and script file paths.
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, 'key-maker.py', None, 1)
+
 
 
 
